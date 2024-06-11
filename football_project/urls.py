53686b7dade7c947b0157aa9e6a717a28abb6a18
calinpff <calin_pff@icloud.com>
2024-06-11 21:03:12 +0300

    First commit

--- football_project/urls.py
+++ football_project/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('', include('players.urls')),
-    path('', include('django.contrib.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
